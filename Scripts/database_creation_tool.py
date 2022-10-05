d8d185deb3b73c07f65c21f44eb0a6ef356fc092
Alexander Nykjær <alax0013@gmail.com>
2022-10-05 09:37:24 +0200

    Minor cleanup.

--- Scripts/database_creation_tool.py
+++ Scripts/database_creation_tool.py
@@ -1,11 +1,13 @@
 import sqlite3
 
+
 def database_creation(database_name):
-    sql = sqlite3.connect("../Databases/"+ database_name + ".sqlite")
+    sql = sqlite3.connect("../Databases/" + database_name + ".sqlite")
     conn = sql.cursor()
     f = open("../SQL/" + database_name + ".sql")
     conn.executescript(str(f.read()))
     sql.commit()
+
 
 database_creation("AdvancedDatabase")
 database_creation("AdvancedDatabaseButBad")
