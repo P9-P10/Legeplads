cec02e3a887f1ec131273bd7e221cbd1953e3aa7
soenderby <jakob.soenderby@hotmail.com>
2022-10-05 11:14:09 +0200

    Refactored database_creation_tool
    Changed it to use a context manager for the database connection.
    Also refactored it to use connection shortcut methods rather than a
    cursor.
    Also explicitly close the connection

--- Scripts/database_creation_tool.py
+++ Scripts/database_creation_tool.py
@@ -1,11 +1,10 @@
 import sqlite3
 
 def database_creation(database_name, database_dir = "../Databases/", script_dir = "../SQL/"):
-    sql = sqlite3.connect(database_dir + database_name + ".sqlite")
-    conn = sql.cursor()
-    with open(script_dir + database_name + ".sql") as f:
-        conn.executescript(read_file_to_string(f))
-    sql.commit()
+    with sqlite3.connect(database_dir + database_name + ".sqlite") as conn:
+        with open(script_dir + database_name + ".sql") as f:
+            conn.executescript(read_file_to_string(f))
+    conn.close()
 
 def read_file_to_string(file):
     return str(file.read())
