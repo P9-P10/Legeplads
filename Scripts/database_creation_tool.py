505fb63f81c1addfd988876bbd57924de9b900b4
soenderby <jakob.soenderby@hotmail.com>
2022-10-05 09:55:29 +0200

    Refactored database_creation_tool
    Used context for opening and using file
    Used default parameters for specifying directories so they can be
    changed easily

--- Scripts/database_creation_tool.py
+++ Scripts/database_creation_tool.py
@@ -1,11 +1,14 @@
 import sqlite3
 
-def database_creation(database_name):
-    sql = sqlite3.connect("../Databases/"+ database_name + ".sqlite")
+def database_creation(database_name, database_dir = "../Databases/", script_dir = "../SQL/"):
+    sql = sqlite3.connect(database_dir + database_name + ".sqlite")
     conn = sql.cursor()
-    f = open("../SQL/" + database_name + ".sql")
-    conn.executescript(str(f.read()))
+    with open(script_dir + database_name + ".sql") as f:
+        conn.executescript(read_file_to_string(f))
     sql.commit()
+
+def read_file_to_string(file):
+    return str(file.read())
 
 database_creation("AdvancedDatabase")
 database_creation("AdvancedDatabaseButBad")
