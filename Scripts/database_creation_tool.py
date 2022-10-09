534ef6c59b2ab0cd3c1b24bc929814789136f4d8
dkalaxdk <alax0013@gmail.com>
2022-10-09 15:04:04 +0200

    Spacing.

--- Scripts/database_creation_tool.py
+++ Scripts/database_creation_tool.py
@@ -1,7 +1,8 @@
 import os.path
 import sqlite3
 
-def database_creation(database_name, database_dir = "../Databases/", script_dir = "../SQL/"):
+
+def database_creation(database_name, database_dir="../Databases/", script_dir="../SQL/"):
     if not os.path.exists(database_dir):
         os.mkdir(database_dir)
     with sqlite3.connect(database_dir + database_name + ".sqlite") as conn:
@@ -9,8 +10,10 @@ def database_creation(database_name, database_dir = "../Databases/", script_dir 
             conn.executescript(read_file_to_string(f))
     conn.close()
 
+
 def read_file_to_string(file):
     return str(file.read())
+
 
 database_creation("AdvancedDatabase")
 database_creation("AdvancedDatabaseButBad")
