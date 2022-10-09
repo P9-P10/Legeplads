5ff3519e099e3e8cbadebba343a2801ca3660cc3
dkalaxdk <alax0013@gmail.com>
2022-10-09 15:03:54 +0200

    Now makes the DataBase dir if it does not exist.

--- Scripts/database_creation_tool.py
+++ Scripts/database_creation_tool.py
@@ -1,6 +1,9 @@
+import os.path
 import sqlite3
 
 def database_creation(database_name, database_dir = "../Databases/", script_dir = "../SQL/"):
+    if not os.path.exists(database_dir):
+        os.mkdir(database_dir)
     with sqlite3.connect(database_dir + database_name + ".sqlite") as conn:
         with open(script_dir + database_name + ".sql") as f:
             conn.executescript(read_file_to_string(f))
