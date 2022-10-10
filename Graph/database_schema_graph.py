473a9b19a745ed1f2959d4bf0fd3a0fa4081703c
soenderby <jakob.soenderby@hotmail.com>
2022-10-10 09:37:20 +0200

    Created simple graph for simple database

---
+++ Graph/database_schema_graph.py
@@ -0,0 +1,67 @@
+from rdflib import Graph, URIRef, Literal, Namespace
+from rdflib.namespace import RDF, RDFS, FOAF, XSD, OWL
+import sqlite3
+
+
+# Could not figure out how to import from Applications folder, so I copied what I needed
+conn = sqlite3.connect('./Databases/SimpleDatabase.sqlite')
+
+def run_query(query):
+    response = conn.execute(query).fetchall()
+    return response
+
+table_details = run_query(""" 
+  SELECT
+    m.name as table_name, 
+    p.name as column_name,
+    p.pk as primary_key
+  FROM 
+    sqlite_master AS m
+  JOIN 
+    pragma_table_info(m.name) AS p
+  ORDER BY 
+    m.name, 
+    p.cid; 
+""")
+
+def remove_table_details(data, table):
+  return [row for row in data if table not in row[0]]
+
+foreign_keys = run_query("""
+  SELECT 
+      m.name, p."from", p."table", p."to"
+  FROM
+      sqlite_master as m
+      JOIN pragma_foreign_key_list(m.name) as p ON m.name != p."table"
+  WHERE m.type = 'table'
+  ORDER BY m.name;
+""")
+
+tables = remove_table_details(table_details, 'sqlite')
+print(tables)
+
+print(foreign_keys)
+
+graph = Graph()
+
+def add_triples_for_tables(graph, rows):
+  unique_table_names = set([row[0] for row in rows])
+
+  for table in unique_table_names:
+    graph.add((URIRef(table), RDF.type, Literal("table")))
+
+
+def add_triples_for_columns(graph, columns):
+  for column in columns:
+    (table, column, is_primary_key) = column
+    graph.add((URIRef(table), Literal("column"), URIRef(column)))
+    if is_primary_key:
+      graph.add((URIRef(column), RDF.type, URIRef("primary_key")))
+
+add_triples_for_tables(graph, tables)
+add_triples_for_columns(graph, tables)
+
+for s, p, o in graph:
+  print(s, p, o)
+
+graph.serialize(destination="database.ttl", format="turtle")
