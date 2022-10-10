dd3bc4d5886dee84fa8d544d570fb014d1c766d6
soenderby <jakob.soenderby@hotmail.com>
2022-10-10 12:24:50 +0200

    Created classes for transforming queries
    Refactored optimizedSqlInterface to use them

--- Applications/optimizedSqlInterface.py
+++ Applications/optimizedSqlInterface.py
@@ -4,15 +4,16 @@ import sqlite3
 from Applications.dbinterface import DBInterface
 from Helpers.changes_class import Changes as Ch
 from Helpers.simple_sql_parser import SqlParser as Sp
+from Applications.querytransformation import RemoveTable, ChangeTable
 
 
 class OptimizedSqliteInterface(DBInterface):
-    def __init__(self, connection):
-        super().__init__(connection)
+    def __init__(self, path_to_database):
+        super().__init__(path_to_database)
         # {table_name:changes_class}
         # {string    :changes      }
         self.changes: dict = {}
-        self.sql = sqlite3.connect(self.connection)
+        self.sql = sqlite3.connect(self.path_to_database)
         self.parser = Sp()
 
     def run_query(self, query: str):
@@ -31,29 +32,7 @@ class OptimizedSqliteInterface(DBInterface):
             if table in self.changes:
                 current_changes: Ch = self.changes[table]
                 if not current_changes.should_rename:
-                    query = self.remove_occurrences_in_joins(self.find_and_remove_alias(query, table), table)
+                    query = RemoveTable(table).apply(query)
                 else:
-                    query = self.replace_occurrences(query, table, current_changes.new_name)
-        return query
-
-    @staticmethod
-    def remove_occurrences_in_joins(query: str, table_name: str):
-        query_list_without_removed_table = [x for x in re.split(r"join", query, flags=re.IGNORECASE) if
-                                            table_name not in x]
-        for i, item in enumerate(query_list_without_removed_table):
-            if not i == 0:
-                query_list_without_removed_table[i] = "JOIN" + item
-        return ''.join(query_list_without_removed_table)
-
-    @staticmethod
-    def replace_occurrences(query: str, table_name: str, new_table_name: str):
-        return re.sub(r'%s(\W|;|$)' % table_name, new_table_name + " ", query)
-
-    @staticmethod
-    def find_and_remove_alias(query: str, table_name: str):
-        query_without_whitespace = query.split(' ')
-        alias_position = query_without_whitespace.index(table_name) + 1
-        if query_without_whitespace[alias_position].lower() != "on":
-            alias = query_without_whitespace[alias_position]
-            return query.replace(alias + ".", "")
+                    query = ChangeTable(table, current_changes.new_name).apply(query)
         return query
--- Tests/test_optimized_interface.py
+++ Tests/test_optimized_interface.py
@@ -1,55 +1,12 @@
 from Applications.optimizedSqlInterface import OptimizedSqliteInterface as OSi
 from Helpers.changes_class import Changes as Ch
 
-
-def test_replace_occurrences():
-    connection = OSi("")
-    query = "SELECT * FROM testTable JOIN other_table"
-    result = connection.replace_occurrences(query, "other_table", "correct_table")
-    assert result == "SELECT * FROM testTable JOIN correct_table "
-
-
-def test_replace_occurrences_does_not_replace_substring():
-    connection = OSi("")
-    query = "SELECT * FROM testTable JOIN other_table_with_longer_name"
-    result = connection.replace_occurrences(query, "other_table", "correct_table")
-    assert result == "SELECT * FROM testTable JOIN other_table_with_longer_name"
-
-
-def test_remove_occurrences_in_joins():
-    connection = OSi("")
-    query = "SELECT * FROM testTable JOIN other_table"
-    result = connection.remove_occurrences_in_joins(query, "other_table")
-    assert result == "SELECT * FROM testTable "
-
-
-def test_remove_occurrences_in_joins_lower_case_JOIN():
-    connection = OSi("")
-    query = "SELECT * FROM testTable join other_table"
-    result = connection.remove_occurrences_in_joins(query, "other_table")
-    assert result == "SELECT * FROM testTable "
-
-
-def test_remove_occurrences_in_joins_mixed_case_JOIN():
-    connection = OSi("")
-    query = "SELECT * FROM testTable Join other_table"
-    result = connection.remove_occurrences_in_joins(query, "other_table")
-    assert result == "SELECT * FROM testTable "
-
-
-def test_find_and_remove_alias():
-    connection = OSi("")
-    query = "SELECT name, ot.other_value FROM testTable JOIN other_table ot"
-    result = connection.find_and_remove_alias(query, "other_table")
-    assert result == "SELECT name, other_value FROM testTable JOIN other_table ot"
-
-
 def test_modify_query_with_change_remove_value():
     connection = OSi("")
     connection.add_database_change('other_table', Ch())
     query = "SELECT name, ot.other_value FROM testTable JOIN other_table ot"
     result = connection.modify_query_with_changes(query)
-    assert result == "SELECT name, other_value FROM testTable "
+    assert result == "SELECT name, other_value FROM testTable"
 
 
 def test_modify_query_with_change_replace_value():
