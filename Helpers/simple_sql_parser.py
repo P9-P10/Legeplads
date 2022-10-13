bc5892e1707bc1b3f6ed8cc35408065ff4ab70ba
Alexander Nykjær <alax0013@gmail.com>
2022-10-13 14:10:27 +0200

    Parser can now find variables. And their alias'

--- Helpers/simple_sql_parser.py
+++ Helpers/simple_sql_parser.py
@@ -4,7 +4,7 @@ import re
 class SqlParser:
     def __init__(self):
         self.keywords = ["join", "into", "from"]
-        self.end_of_table_list_keywords = ["where", "join","from"]
+        self.end_of_table_list_keywords = ["where", "join", "from", "select"]
         self.keywords_before_tables = ["join", "from"]
         self.query_types = ["select", "delete", "update"]
 
@@ -20,60 +20,64 @@ class SqlParser:
     def get_where_clause(query):
         return re.split(r"where", query, 1, flags=re.IGNORECASE)[1]
 
-    def get_variables_without_table_prefixes(self, query):
+    def get_variables_with_prefix(self, query):
         output = []
-        query_without_spaces = query.split(" ")
+        query_without_spaces = self.split_query_components(query)
         in_variable_section = False
-        for index, item in enumerate(query_without_spaces):
-            if item.lower() in self.query_types or in_variable_section:
+        index = 0
+        while index < len(query_without_spaces):
+            if query_without_spaces[index].lower() in self.query_types:
                 in_variable_section = True
                 index += 1
-                current_variable = query_without_spaces[index]
-                cleaned_current_var = self.remove_seperator(self.split_prefix(current_variable)[1])
-                if query_without_spaces[index + 1].lower() in self.end_of_table_list_keywords:
-                    output.append(cleaned_current_var)
-                    index += 1
-                    break
-                elif query_without_spaces[index + 1].lower() == "as":
-                    element_after_as = query_without_spaces[index+2]
-                    output.append(self.remove_seperator(self.split_prefix(element_after_as)[1]))
-                    index += 2
-                else:
-                    output.append(cleaned_current_var)
-                    index += 1
+                continue
+            current_variable = query_without_spaces[index]
+            cleaned_current_var = self.split_prefix(current_variable)
+            next_element = self.get_n_element(query_without_spaces, 1, index)
+            if next_element.lower() in self.end_of_table_list_keywords:
+                output.append(cleaned_current_var)
+                index += 1
+                break
+            elif next_element == "as":
+                element_after_as = self.get_n_element(query_without_spaces, 2, index)
+                output.append(self.split_prefix(element_after_as))
+                index += 3
+            else:
+                output.append(cleaned_current_var)
+                index += 1
             if index + 1 >= len(query_without_spaces):
                 break
         return output
 
     @staticmethod
-    def remove_seperator(input_string: str):
-        if ',' in input_string:
-            return input_string.replace(',', '')
-        else:
-            return input_string
+    def get_n_element(list, n, index):
+        return list[index + n]
 
     @staticmethod
     def split_prefix(input_string: str):
+
         if '.' in input_string:
-            return input_string.split('.')
+            split_string = input_string.split('.')
+            return split_string[0], split_string[1]
         else:
-            return ['', input_string]
+            return '', input_string
 
     def get_table_alias(self, query):
         output = []
-        query_without_spaces = query.split(" ")
+        query_without_spaces = self.split_query_components(query)
         for index, item in enumerate(query_without_spaces):
             if item.lower() in self.keywords_before_tables:
                 index += 1
                 current_table_name = query_without_spaces[index]
-                if query_without_spaces[index + 1].lower() in self.end_of_table_list_keywords:
+                next_element = query_without_spaces[index + 1]
+                if next_element.lower() in self.end_of_table_list_keywords:
                     output.append((current_table_name, ""))
                     index += 1
-                elif query_without_spaces[index + 1].lower() == "as":
-                    output.append((current_table_name, query_without_spaces[index + 2]))
+                elif next_element == "as":
+                    alias = query_without_spaces[index + 2]
+                    output.append((current_table_name, alias))
                     index += 2
                 else:
-                    output.append((current_table_name, query_without_spaces[index + 1]))
+                    output.append((current_table_name, next_element))
                     index += 1
         return output
 
@@ -81,3 +85,7 @@ class SqlParser:
         query_without_spaces = query.split(" ")
         if query_without_spaces[0].lower() in self.query_types:
             return query_without_spaces[0].upper()
+
+    @staticmethod
+    def split_query_components(query):
+        return re.split(r', | |,', query)
