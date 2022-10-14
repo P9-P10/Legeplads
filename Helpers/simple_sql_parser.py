108765eab7b7465cb21e22f5b191ac9a728bc5a7
soenderby <jakob.soenderby@hotmail.com>
2022-10-14 14:28:08 +0200

    Refactored for readability

--- Helpers/simple_sql_parser.py
+++ Helpers/simple_sql_parser.py
@@ -26,23 +26,26 @@ class SqlParser:
             if query.is_query_type(component):
                 continue
                 
-            next_element = query.peek()
-            if next_element == "as":
-                variable = query.next(2)
-            else:
-                variable = component
-            output.append(self.split_prefix(variable))
+            prefix_and_variable = self.get_next_variable_and_prefix(query, component)
+            output.append(self.split_prefix_and_variable(prefix_and_variable))
 
-            if self.is_keyword(next_element):
+            if self.is_keyword(query.peek()):
                 return output
    
         return output
+
+    def get_next_variable_and_prefix(self, query, component):
+        if query.peek() == "as":
+            prefix_and_variable = query.next(2)
+        else:
+            prefix_and_variable = component
+        return prefix_and_variable
 
     def is_keyword(self, string: str) -> bool:
         return string.lower() in self.SQL_keywords
 
     @staticmethod
-    def split_prefix(input_string: str):
+    def split_prefix_and_variable(input_string: str):
         if '.' in input_string:
             return tuple(input_string.split('.'))
         else:
@@ -54,15 +57,20 @@ class SqlParser:
 
         for component in query:
             if self.is_prefix(component):
-                table = query.next()
-                alias = query.peek()
-
-                if self.is_keyword(alias):
-                    value = ""
-                else:
-                    value = alias
-
-                output.append((table, value))
+                output.append(self.get_table_and_alias(query))
 
         return output
+
+    def get_table_and_alias(self, query):
+        table = self.get_table(query)
+        value = self.get_alias(query)
+        return table, value
+
+    def get_table(self, query):
+        return query.next()
+
+    def get_alias(self, query):
+        next_value = query.peek()
+        return "" if self.is_keyword(next_value) else next_value
+  
 
