f359c5305ce479b1e5bd2f77f92edef549e7c35a
Alexander Nykjær <alax0013@gmail.com>
2022-10-14 12:15:35 +0200

    Added test for abnormal amount of spaces

--- Tests/test_query.py
+++ Tests/test_query.py
@@ -34,3 +34,15 @@ def test_split_query_components_contains_newLine():
     print(query.split_query_components())
     assert query.split_query_components() == ['SELECT', '*', 'FROM', 'Users', 'JOIN', 'Orders', 'O', 'on', 'Users.id',
                                               '=', 'O.owner', 'WHERE', 'O.owner', '=', "'bob'"]
+
+def test_split_query_components_contains_abnormal_amount_of_spaces():
+    query = Q("""SELECT * FROM Users                                                 JOIN Orders O on
+    Users.id = O.owner           
+    WHERE               O.owner = 'bob'""")
+    print(query.split_query_components())
+    assert query.split_query_components() == ['SELECT', '*',
+                                              'FROM',
+                                              'Users',
+                                              'JOIN',
+                                              'Orders', 'O', 'on', 'Users.id',
+                                              '=', 'O.owner', 'WHERE', 'O.owner', '=', "'bob'"]
