25c6d46f3bf3a1515077cacccad2897ab84f15b5
soenderby <jakob.soenderby@hotmail.com>
2022-10-24 13:03:37 +0200

    Undid creation of AST and moved changes to query

--- Applications/DatabaseRepresenations/Query.py
+++ Applications/DatabaseRepresenations/Query.py
@@ -1,6 +1,5 @@
 from Applications.DatabaseRepresenations.Table import Table
 from Applications.DatabaseRepresenations.Column import Column
-from Applications.DatabaseRepresenations.ast import AST
 
 import sqlglot
 
@@ -11,7 +10,7 @@ class Query:
     def __init__(self, query_as_string: str):
         self.query_as_string = query_as_string
         try:
-            self.ast = AST(parse_one(query_as_string))
+            self.ast = parse_one(query_as_string)
         except sqlglot.ParseError:
             raise ValueError("The query is not valid SQL")
 
@@ -40,10 +39,108 @@ class Query:
             new_table = change.get_new_table()
             old_table = change.get_old_table()
 
-            self.ast.replace_table(old_table, new_table)
+            self.replace_table(old_table, new_table)
 
     def fully_qualify_column_names(self, tables: list[Table]):
-        self.ast.create_needed_aliases()
-        alias_map = self.ast.create_alias_map()
-        self.ast.apply_missing_aliases(tables, alias_map)
+        self.create_needed_aliases()
+        alias_map = self.create_alias_map()
+        self.apply_missing_aliases(tables, alias_map)
 
+    def transform_ast(self, transformer):
+        self.ast = self.ast.transform(transformer)
+
+    def replace_table(self, old_table: Table, new_table: Table):
+        def transform(node):
+            if isinstance(node, exp.Table) and node.name == str(old_table):
+                return parse_one(str(new_table))
+            return node
+
+        self.transform_ast(transform)
+
+    def create_alias_map(self):
+        alias_map = {}
+        for alias in self.get_aliases():
+            self.add_alias_if_exists(alias, alias_map)
+        return alias_map
+
+    def get_aliases(self):
+        return self.get_all_instances(exp.Alias)
+
+    def get_all_instances(self, type):
+        return self.ast.find_all(type)
+
+    def add_alias_if_exists(self, alias, alias_map):
+        table = self.get_table(alias)
+        if table:
+            self.add_alias(alias, alias_map, table)
+
+    def add_alias(self, alias, alias_map, table):
+        table_alias = self.get_table_alias(alias)
+        alias_map[table.name] = table_alias.name
+
+    def get_table(self, object):
+        return self.get_next_instance(object, exp.Table)
+
+    def get_table_alias(self, object):
+        return self.get_next_instance(object, exp.TableAlias)
+
+    def get_next_instance(self, object, type):
+        return object.find(type)
+
+    def apply_missing_aliases(self, tables, alias_map):
+        if not tables:
+            return
+        
+        def transformer(node):
+            if isinstance(node, exp.Column):
+                if self.node_has_no_table(node):
+                    return self.apply_alias_to_node(node, self.get_alias_for_column(tables, alias_map, node.name))
+                elif node.table in [table.name for table in tables]:
+                    return self.apply_alias_to_node(node, self.get_alias_for_table(node.table, alias_map))
+            return node
+
+        self.transform_ast(transformer)
+
+    def node_has_no_table(self, node):
+        return node.table == ""
+
+    def apply_alias_to_node(self, node, alias):
+        return node.replace(self.create_column(node.name, alias))
+
+    def create_column(self, name, table):
+        return exp.Column(
+            this=self.create_identifier(name),
+            table=self.create_identifier(table)
+        )
+
+    def create_identifier(self, name: str):
+        return exp.Identifier(this=name)
+
+    def create_needed_aliases(self):
+        def transform(node):
+            if isinstance(node, exp.Table):
+                if not isinstance(node.parent, exp.Alias):
+                    return self.create_alias_on_node(node)
+            return node
+
+        self.transform_ast(transform)
+
+    def create_alias_on_node(self, node):
+        return node.replace(exp.Alias(
+            this=exp.Table(
+                this=exp.Identifier(this=node.name, quoted=False)),
+            alias=exp.TableAlias(
+                this=exp.Identifier(this=node.name + "1"))
+        ))
+
+    def get_alias_for_table(self, table_name, table_map):
+        return table_map[table_name]
+
+    def get_alias_for_column(self, tables, aliases, column_name):
+        column_table = self.get_table_for_column(tables, column_name)
+        return aliases[column_table]
+
+    def get_table_for_column(self, tables, column_name):
+        for table in tables:
+            if table.has_column(Column(column_name)):
+                return table.name
--- Tests/test_ast.py
+++
@@ -1,4 +0,0 @@
-from Applications.DatabaseRepresenations.ast import AST
-
-def test_can_create_instance_of_ast():
-    AST(None)
