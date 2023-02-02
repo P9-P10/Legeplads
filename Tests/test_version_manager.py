e6d427f78a51e4006c0288fcb1b4ec9a1b7963bc
Jakob Sønderby Kristensen <33911621+soenderby@users.noreply.github.com>
2023-02-02 13:50:50 +0100

    Removed duplication from test_version_manager.py using fixtures

--- Tests/test_version_manager.py
+++ Tests/test_version_manager.py
@@ -1,9 +1,7 @@
-import json
-
+import pytest
 from Graph.changes_parser import ChangesParser
 from Graph.graph_parser import GraphParser
 from Graph.versionmanager import VersionManager
-from Structures.Changes import MoveColumn
 from Structures.Column import Column
 from Structures.DataStore import DataStore
 from Structures.Schema import Schema
@@ -14,7 +12,7 @@ class Moc_GraphParser(GraphParser):
     def __init__(self):
         super().__init__()
 
-    def get_structure(self, string_to_parse: str) -> [DataStore]:
+    def get_structure(self, string_to_parse: str) -> list[DataStore]:
         return string_to_parse
 
 
@@ -39,36 +37,37 @@ def create_version_manager(old_schema, new_schema):
     return VersionManager(gp, cp, [old_tuple, new_tuple], [changes_tuple])
 
 
-def test_init():
-    old_schema = Schema([Table("TableName", [Column("TestName", uri="1")])], name="SchemaName")
-    new_schema = Schema([Table("TableName", [Column("NewColumnName", uri="2")])], name="SchemaName")
-    vm = create_version_manager(old_schema, new_schema)
-    assert VersionManager.Change(1, old_schema, new_schema, "Id") in vm.changes
+@pytest.fixture
+def old_schema():
+    return Schema([Table("TableName", [Column("TestName", uri="1")])], name="SchemaName")
+
+@pytest.fixture
+def new_schema():
+    return Schema([Table("TableName", [Column("NewColumnName", uri="2")])], name="SchemaName")
 
 
-def test_get_changes_for_column():
-    old_schema = Schema([Table("TableName", [Column("TestName", uri="1")])], name="SchemaName")
-    new_schema = Schema([Table("TableName", [Column("NewColumnName", uri="2")])], name="SchemaName")
-    vm = create_version_manager(old_schema, new_schema)
-
-    assert vm.get_change_for_column("TableName", "TestName", version=1, schema_name="SchemaName", ) == new_schema
+@pytest.fixture
+def version_manager(old_schema, new_schema):
+    return create_version_manager(old_schema, new_schema)
 
 
-def test_get_changes_for_column_wrong_value_returns_none():
-    old_schema = Schema([Table("TableName", [Column("TestName", uri="1")])], name="SchemaName")
-    new_schema = Schema([Table("TableName", [Column("NewColumnName", uri="2")])], name="SchemaName")
-    vm = create_version_manager(old_schema, new_schema)
 
-    assert vm.get_change_for_column("Non Existing", "Non Existing", version=1,
+def test_init(old_schema, new_schema, version_manager):
+    assert VersionManager.Change(1, old_schema, new_schema, "Id") in version_manager.changes
+
+
+def test_get_changes_for_column(new_schema, version_manager):
+    assert version_manager.get_change_for_column("TableName", "TestName", version=1, schema_name="SchemaName", ) == new_schema
+
+
+def test_get_changes_for_column_wrong_value_returns_none(version_manager):
+    assert version_manager.get_change_for_column("Non Existing", "Non Existing", version=1,
                                     schema_name="Non Existing") is None
 
 
-def test_get_datastore_for_change():
-    old_schema = Schema([Table("TableName", [Column("TestName", uri="1")])], name="SchemaName")
-    new_schema = Schema([Table("TableName", [Column("NewColumnName", uri="2")])], name="SchemaName")
-    vm = create_version_manager(old_schema, new_schema)
+def test_get_datastore_for_change(old_schema, new_schema, version_manager):
     old_ds = DataStore([old_schema], name="Name")
     new_ds = DataStore([new_schema], name="Name")
 
-    result = vm.get_data_stores_for_change(version=1, database_id="Id")
+    result = version_manager.get_data_stores_for_change(version=1, database_id="Id")
     assert result == (old_ds, new_ds)
