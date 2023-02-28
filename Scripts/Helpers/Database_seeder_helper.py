eaf87040ab2364baad63057ef42e3ef5f3d795b1
Alexander Nykjær <alax0013@gmail.com>
2023-02-28 09:01:37 +0100

    Cleanup of code

--- Scripts/Helpers/Database_seeder_helper.py
+++ Scripts/Helpers/Database_seeder_helper.py
@@ -1,12 +1,36 @@
-import random
 import datetime
+import random
 
 from dateutil.relativedelta import relativedelta
+from randomuser import RandomUser
 
-from Scripts.database_seeder import User
 from Scripts.Helpers.database_seeder_formatters import *
 
 
+class User:
+    def __init__(self, user_id, c_user):
+        self.username = toSQLString(c_user.get_username())
+        self.password = toSQLString(c_user.get_login_md5())
+        self.email = toSQLString(c_user.get_email())
+        self.first_name = toSQLString(c_user.get_first_name())
+        self.last_name = toSQLString(c_user.get_last_name())
+        self.birthday = toSQLString(c_user.get_dob())
+        self.subscribed = bool(random.getrandbits(1))
+        self.address = toSQLString(c_user.get_street())
+        self.phone_number = toSQLString(c_user.get_phone())
+        self.id = user_id
+        self.name = toSQLString(c_user.get_first_name() + " " + c_user.get_last_name())
+
+
+def generate_users(count=10) -> [User]:
+    users = []
+    user_list = RandomUser.generate_users(count, {"nat": "dk"})
+    g_id = 0
+    for c_user in user_list:
+        id = g_id + 1
+        g_id = g_id + 1
+        users.append(User(id, c_user))
+    return users
 
 
 def get_random_product():
@@ -32,9 +56,21 @@ def random_date():
     random_second = random.randrange(int_delta)
     return toSQLString(current_date - datetime.timedelta(seconds=random_second))
 
+
 def get_random_user_id(users: [User]):
     return random.randint(0, len(users) - 1) + 1
 
 
 def get_random_user_address(users: [User]):
     return users[random.randint(0, len(users) - 1)].address
+
+
+
+def generate_extra_newsletters(users:[User]):
+    user = users.pop()
+
+    return user.email, user.subscribed
+
+
+def get_random_user(users):
+    return users[random.randint(0, len(users) - 1)]
--- Scripts/database_seeder.py
+++ Scripts/database_seeder.py
@@ -1,40 +1,11 @@
-import random
-
-from randomuser import RandomUser
 from Scripts.Helpers.Database_seeder_helper import *
 from Scripts.Helpers.database_seeder_formatters import *
 
-
-class User():
-    def __init__(self, id, c_user):
-        self.username = toSQLString(c_user.get_username())
-        self.password = toSQLString(c_user.get_login_md5())
-        self.email = toSQLString(c_user.get_email())
-        self.first_name = toSQLString(c_user.get_first_name())
-        self.last_name = toSQLString(c_user.get_last_name())
-        self.birthday = toSQLString(c_user.get_dob())
-        self.subscribed = bool(random.getrandbits(1))
-        self.address = toSQLString(c_user.get_street())
-        self.phone_number = toSQLString(c_user.get_phone())
-        self.id = id
-        self.name = toSQLString(c_user.get_first_name() + " " + c_user.get_last_name())
-
-
 column_type_map = {"subscribed": "BOOL",
-                   "id": "INT NOT NULL PRIMARY KEY",
+                   "id": "INTEGER PRIMARY KEY AUTOINCREMENT",
                    "order_date": "DATETIME",
                    "orderedBy": "INT",
                    "birthday": "DATETIME"}
-
-def generate_users(count=10):
-    users = []
-    user_list = RandomUser.generate_users(count, {"nat": "dk"})
-    g_id = 0
-    for c_user in user_list:
-        id = g_id + 1
-        g_id = g_id + 1
-        users.append(User(id, c_user))
-    return users
 
 
 def create_table(table_name, column_type: [str]):
@@ -45,9 +16,9 @@ def create_table(table_name, column_type: [str]):
         else:
             output_string = generate_row_creation(output_string, column_name, "VARCHAR(256)")
     output_string = remove_last_comma(output_string)
-    output_string = output_string + "\n);"
+    output_string = output_string + "\n); \n \n"
 
-    print(output_string)
+    return output_string
 
 
 def populate_table_from_users(table_name, users: [User], columns: [str], user_properties: [str]):
@@ -58,13 +29,15 @@ def populate_table_from_users(table_name, users: [User], columns: [str], user_pr
             output_string = apply_user_data(property, output_string, user)
         output_string = end_of_line_format(output_string)
     output_string = end_of_values_format(output_string)
-    print(output_string)
+    return output_string
 
 
-def populate_table_from_functions(table_name, functions: [], columns: [str], count: int, id_first=True):
+def populate_table_from_functions(table_name, functions: [], columns: [str], count: int, id_first=True, start_id=0):
     output_string = insert_into(columns, table_name)
 
-    for i in range(count):
+    count = start_id + count
+
+    for i in range(start_id, count):
         output_string = output_string + "("
         if id_first:
             output_string = print_id(i, output_string)
@@ -72,39 +45,65 @@ def populate_table_from_functions(table_name, functions: [], columns: [str], cou
             output_string = apply_function(f, output_string)
         output_string = end_of_line_format(output_string)
     output_string = end_of_values_format(output_string)
-    print(output_string)
+    return output_string
 
 
 def apply_function(f, output_string):
     result = f()
     if isinstance(result, tuple):
-        output_string = output_string + line_format(result[0]) + line_format(result[1])
+        output_string += line_format(str(result[0])) + line_format(str(result[1]))
     else:
-        output_string = output_string + line_format(str(result))
+        output_string += line_format(str(result))
     return output_string
 
 
-def define_all_tables(count=10):
+def drop_table(table):
+    return "DROP TABLE IF EXISTS " + table + "; \n"
+
+
+def define_all_tables(count=10, should_drop_table=False):
     users = generate_users(count)
+    second_user_list = generate_users(count)
     user_table_name = "users"
     newsletter_table_name = "newsletter"
     orders_table_name = "orders"
     user_columns = ["id", "Username", "Password", "Name", "Address"]
     newsletter_columns = ["id", "email", "subscribed"]
     orders_columns = ["id", "products", "order_date", "delivery_address", "orderedBy"]
+    output = ""
 
     order_functions = [lambda: get_random_product(),
                        lambda: random_date(),
                        lambda: get_random_adress_and_ordered_by(users)]
 
-    create_table(user_table_name, user_columns)
-    create_table(newsletter_table_name, newsletter_columns)
-    create_table(orders_table_name, orders_columns)
+    newsletter_functions = [lambda: generate_extra_newsletters(second_user_list)]
 
-    populate_table_from_users(user_table_name, users, user_columns, ["id", "username", "password", "name", "address"])
-    populate_table_from_users(newsletter_table_name, users, newsletter_columns, ["id", "email", "subscribed"])
-    populate_table_from_functions(orders_table_name, order_functions, orders_columns, count * 5)
+    if should_drop_table:
+        output += drop_table(user_table_name)
+        output += drop_table(newsletter_table_name)
+        output += drop_table(orders_table_name)
+
+    output += create_table(user_table_name, user_columns)
+    output += create_table(newsletter_table_name, newsletter_columns)
+    output += create_table(orders_table_name, orders_columns)
+
+    output += populate_table_from_users(user_table_name, users, user_columns,
+                                        ["id", "username", "password", "name", "address"])
+    output += populate_table_from_users(newsletter_table_name, users, newsletter_columns, ["id", "email", "subscribed"])
+    output += populate_table_from_functions(orders_table_name, order_functions, orders_columns, count * 5)
+    # The below call ensures that there are more entries into newsletter than there are in users
+    output += populate_table_from_functions(newsletter_table_name, newsletter_functions, newsletter_columns, count,
+                                            start_id=count)
+
+    return output
 
 
 if __name__ == "__main__":
-    define_all_tables(20)
+    should_print = False
+    filepath = "output.sql"
+    output = define_all_tables(5000, True)
+    if should_print:
+        print(output)
+    else:
+        with open(filepath, "w", encoding="utf-8") as f:
+            f.write(output)
